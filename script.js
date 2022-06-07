f78f7e4a09a1afd61f672b71b43cd9d80e494f15
Andras Varga <zoryandras@gmail.com>
2022-06-07 22:13:08 +0200

    Did the index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,105 @@
+const quizData = [
+    {
+        question: 'Who is the most lovable weirdo out there?',
+        a: 'Me',
+        b: 'You',
+        c: 'No one',
+        d: 'András Varga',
+        correct: 'd'
+    }, {
+        question: 'Who is Bori?',
+        a: 'Hipster Jesus',
+        b: 'Put out a glass full of vodka soda and see who picks it up first',
+        c: 'Bori?',
+        d: 'András Varga',
+        correct: 'b'
+    }, {
+        question: 'Best doctor ever?',
+        a: 'Andor baba',
+        b: 'Doctor Who',
+        c: 'The Green one',
+        d: 'Vodka',
+        correct: 'a'
+    }, {
+        question: 'What fears Cintia the most?',
+        a: 'András Varga',
+        b: 'The Delete Button',
+        c: 'Kitten paws',
+        d: 'Random screeching sounds',
+        correct: 'b'
+    }, {
+        question: 'What instrument Dávid plays?',
+        a: 'Tabletop',
+        b: 'Shoestrings',
+        c: 'Guitar',
+        d: 'Moustache of alien gladiators',
+        correct: 'c'
+    }
+]
+
+
+const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+  let answer = undefined;
+
+  answerElements.forEach((answerElement) => {
+    if (answerElement.checked) {
+      answer = answerElement.id;
+    }
+  });
+
+  return answer;
+}
+
+function deselectAnswers() {
+  answerElements.forEach((answerElement) => {
+    answerElement.checked = false;
+  });
+}
+
+submitBtn.addEventListener("click", () => {
+  // check to see the answer
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+                <h2 id="results">Here are your results: ${score}/${quizData.length}. Crazy, innit?!</h2>
+                
+                <button onclick="location.reload()">Again! AGAINNNN!!!</button>
+            `;
+    }
+  }
+});
